34514e3523bbb937ea3ff8b7b2d2395cbe2745c7
RipetaShane <shane@ripeta.com>
2023-07-21 11:48:23 +0100

    Updated yaml file to commit changes to repo

--- forecast_checker.R
+++ forecast_checker.R
@@ -153,5 +153,5 @@ bound <- bind_rows(
   )
 
 #export
-write.table(bound, "data/forecast_check.csv", append = T, row.names = F, sep = ",")
+write.table(bound, "data/forecast_check.csv", append = T, row.names = F, sep = ",", col.names = F)
   
